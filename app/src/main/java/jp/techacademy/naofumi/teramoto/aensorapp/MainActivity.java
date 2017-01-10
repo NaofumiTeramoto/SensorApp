d77affa3232f38627a3c75241e3e497b1a6c355e
NaofumiTeramoto <nfteramoto@gmail.com>
2017-01-11 00:19:46 +0900

    ファイル保存追加中

--- app/src/main/java/jp/techacademy/naofumi/teramoto/aensorapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/naofumi/teramoto/aensorapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,7 +13,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.text.DecimalFormat;
+
+import static android.os.Environment.getDataDirectory;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
@@ -90,8 +99,29 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
                 textViewX.setText("X方向 "+df1.format(e.values[0]));
                 textViewY.setText("Y方向 "+df1.format(e.values[1]));
                 textViewZ.setText("Z方向 "+df1.format(e.values[2]));
+
+                String et = new String(df1.format(e.values[0])+","+df1.format(e.values[1])+","+df1.format(e.values[2]));
+
                 if(flag==0) {
-//                    tv[2].setText(String.valueOf(flag));
+                    try {
+
+                        String filePath = Environment.getDataDirectory()+"/test.txt";
+                        File file = new File(filePath);
+                        file.getParentFile().mkdir();
+
+                        FileOutputStream fos =
+                                openFileOutput(filePath, Context.MODE_PRIVATE);
+                        BufferedWriter bw =
+                                new BufferedWriter(
+                                        new OutputStreamWriter(fos));
+                        bw.write(et);
+                        bw.flush();
+                        fos.close();
+                    } catch (FileNotFoundException ee) {
+
+                    } catch (IOException ee) {
+
+                    }
                 }
                 else {
 //                    tv[2].setText(String.valueOf(flag));
@@ -99,4 +129,17 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
             }
         }
         public void onAccuracyChanged(Sensor s,int accuracy){}
-    }}
+    }
+    // ファイルを保存
+    public void saveFile(String file, String str) {
+        FileOutputStream fileOutputstream = null;
+
+        try {
+            fileOutputstream = openFileOutput(file, Context.MODE_PRIVATE);
+            fileOutputstream.write(str.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
